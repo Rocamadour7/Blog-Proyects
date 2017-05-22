6480ce89dcea8ecedbefd196946aeda2120c8e4d
Luis Molina <luis_molina@outlook.com>
2017-05-22 03:32:29 -0600

    Added favicon path.

--- resources/views/partialsadmin/_head.blade.php
+++ resources/views/partialsadmin/_head.blade.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Dashboard</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 <link rel="stylesheet" type='text/css' href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons'>
 <link rel="stylesheet" href="/css/app.css">
--- resources/views/partialsauth/_head.blade.php
+++ resources/views/partialsauth/_head.blade.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>BlogMolina.mx @yield('title')</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
